c2a47234af8c7307259ef5dcadb2f18dc224fc5d
Marek Dostál <dstlmrk@gmail.com>
2025-03-12 16:44:39 +0100

    Bugfix: Test cache busting 3

--- ultihub/core/templatetags/webpack_static.py
+++ ultihub/core/templatetags/webpack_static.py
@@ -1,5 +1,4 @@
 import json
-import logging
 from pathlib import Path
 
 from django import template
@@ -7,8 +6,6 @@ from django.conf import settings
 
 register = template.Library()
 _manifest_cache = None
-
-logger = logging.getLogger(__name__)
 
 
 @register.simple_tag
@@ -19,7 +16,7 @@ def webpack_static(filename: str) -> str:
     """
     global _manifest_cache
     if _manifest_cache is None:
-        manifest_path = Path(settings.BASE_DIR) / "static" / "dist" / "manifest.json"
+        manifest_path = Path(settings.STATIC_ROOT) / "dist" / "manifest.json"
         try:
             with manifest_path.open("r", encoding="utf-8") as f:
                 _manifest_cache = json.load(f)
@@ -27,6 +24,4 @@ def webpack_static(filename: str) -> str:
             _manifest_cache = {}
 
     hashed_filename = _manifest_cache.get(filename, filename)
-    logger.info(_manifest_cache)
     return hashed_filename
-    # return f"{settings.STATIC_URL}dist/{hashed_filename}"
