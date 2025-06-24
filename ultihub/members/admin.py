c13356d7d83c49a43fabad09176baa2ead14af77
Marek Dostál <dstlmrk@gmail.com>
2025-06-25 00:08:01 +0200

    Bugfix: Slow members admin table

--- ultihub/members/admin.py
+++ ultihub/members/admin.py
@@ -9,24 +9,38 @@ from django_countries.fields import Country
 from rangefilter.filters import DateRangeFilterBuilder
 from tournaments.admin import MemberAtTournamentInline
 
-from members.helpers import get_member_participation_counts_for_last_season
+# from members.helpers import get_member_participation_counts_for_last_season
 from members.models import CoachLicence, Member, MemberSexEnum
 
 
 class CitizenshipFilter(admin.SimpleListFilter):
-    title = "citizenship"
+    title = "Citizenship"
     parameter_name = "citizenship"
 
+    _cached_lookups = None
+
+    @classmethod
+    def get_lookups(cls, model_admin: Any) -> list[tuple[str, str]]:
+        if cls._cached_lookups is not None:
+            return cls._cached_lookups  # type: ignore[unreachable]
+        cls._cached_lookups = [
+            (code, Country(code).name)
+            for code in model_admin.model.objects.values_list("citizenship", flat=True).distinct()
+            if code
+        ]
+        return cls._cached_lookups
+
     def lookups(self, request: HttpRequest, model_admin: Any) -> list:
-        unique_citizenships = model_admin.model.objects.values_list(
-            "citizenship", flat=True
-        ).distinct()
-        return [(code, Country(code).name) for code in unique_citizenships if code]
+        return self.get_lookups(model_admin)
 
     def queryset(self, request: HttpRequest, queryset: QuerySet) -> QuerySet:
         if self.value():
             return queryset.filter(citizenship=self.value())
         return queryset
+
+    @classmethod
+    def clear_cache(cls) -> None:
+        cls._cached_lookups = None
 
 
 class CoachLicenceFilter(admin.SimpleListFilter):
@@ -49,21 +63,25 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "original_id",
-        "club__name",
+        "club",
+        "has_coach_licence",
         "first_name",
         "last_name",
-        "_birth_date",
-        "birth_number",
-        "_sex",
-        "_citizenship",
-        "email",
-        "_has_email_confirmed",
-        "has_marketing_consent_given",
-        "has_coach_licence",
-        "_created_at",
-        "participation_count",
+        "birth_year",
+        # "birth_number",
+        # "sex_abbreviation",
+        "citizenship_code",
+        # "email",
+        "has_email_confirmed",
+        # "has_marketing_consent_given",
+        "created_at_local_time",
+        # "participation_count",
     )
-    readonly_fields = ["email_confirmation_token", "email_confirmed_at", "participation_count"]
+    readonly_fields = [
+        "email_confirmation_token",
+        "email_confirmed_at",
+        # "participation_count",
+    ]
 
     actions = ["export_as_csv"]
     ordering = ["-id"]
@@ -79,48 +97,52 @@ class MemberAdmin(admin.ModelAdmin):
         "club__name",
     ]
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        self.member_participation: dict = {}
+    # def __init__(self, *args: Any, **kwargs: Any) -> None:
+    #     super().__init__(*args, **kwargs)
+    #     self.member_participation: dict = {}
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
-        # Calculate member participation count
-        self.member_participation = get_member_participation_counts_for_last_season()
+        # self.member_participation = get_member_participation_counts_for_last_season()
 
+        CitizenshipFilter.clear_cache()
         current_date = now().date()
-        qs = super().get_queryset(request)
-        qs = qs.select_related("club").annotate(
-            has_coach_licence=Exists(
-                CoachLicence.objects.filter(
-                    member=OuterRef("pk"),
-                    valid_from__lte=current_date,
-                    valid_to__gte=current_date,
-                )
-            ),
+        return (
+            super()
+            .get_queryset(request)
+            .select_related("club")
+            .annotate(
+                has_coach_licence=Exists(
+                    CoachLicence.objects.filter(
+                        member=OuterRef("pk"),
+                        valid_from__lte=current_date,
+                        valid_to__gte=current_date,
+                    )
+                ),
+            )
         )
-        return qs
 
-    @admin.display(description="Participation count")
-    def participation_count(self, obj: Member) -> int:
-        """
-        Return the number of days the member has participated
-        in tournaments in current season
-        """
-        return self.member_participation[obj.id]
+    # @admin.display(description="Participation count")
+    # def participation_count(self, obj: Member) -> int:
+    #     """
+    #     Return the number of days the member has participated
+    #     in tournaments in current season
+    #     """
+    #     return self.member_participation[obj.id]
 
     @admin.display(ordering="birth_date")
-    def _birth_date(self, obj: Member) -> str:
-        return obj.birth_date.strftime("%Y/%m/%d")
+    def birth_year(self, obj: Member) -> str:
+        return obj.birth_date.strftime("%Y")
 
-    def _citizenship(self, obj: Member) -> str:
+    @admin.display(description="Citizenship")
+    def citizenship_code(self, obj: Member) -> str:
         return obj.citizenship.code
 
-    @admin.display(
-        description="Created",
-        ordering="created_at",
-    )
-    def _created_at(self, obj: Member) -> str:
-        return localtime(obj.created_at).strftime("%Y/%d/%m %H:%M")
+    def club(self, obj: Member) -> str:
+        return obj.club.short_name_or_name
+
+    @admin.display(description="Created")
+    def created_at_local_time(self, obj: Member) -> str:
+        return localtime(obj.created_at).strftime("%Y/%m/%d %H:%M")
 
     @admin.display(description="Marketing consent", boolean=True)
     def has_marketing_consent_given(self, obj: Member) -> bool:
@@ -131,10 +153,11 @@ class MemberAdmin(admin.ModelAdmin):
         return obj.has_coach_licence  # type: ignore
 
     @admin.display(description="Confirmed email", boolean=True)
-    def _has_email_confirmed(self, obj: Member) -> bool:
+    def has_email_confirmed(self, obj: Member) -> bool:
         return obj.has_email_confirmed
 
-    def _sex(self, obj: Member) -> str:
+    @admin.display(description="Sex")
+    def sex_abbreviation(self, obj: Member) -> str:
         return "F" if obj.sex == MemberSexEnum.FEMALE else "M"
 
     @admin.display(description="Export selected")
@@ -156,7 +179,7 @@ class MemberAdmin(admin.ModelAdmin):
             "email_confirmed_at",
             "marketing_consent_given_at",
             "has_coach_licence",
-            "participation_count",
+            # "participation_count",
             "created_at",
         ]
 
@@ -184,8 +207,8 @@ class MemberAdmin(admin.ModelAdmin):
                     obj.email_confirmed_at,
                     obj.marketing_consent_given_at,
                     obj.has_coach_licence,
-                    self.member_participation[obj.id],
-                    self._created_at(obj),
+                    # self.member_participation[obj.id],
+                    self.created_at_local_time(obj),
                 ]
             )
 
--- ultihub/users/context_processors.py
+++ ultihub/users/context_processors.py
@@ -4,6 +4,9 @@ from users.services import get_user_managed_clubs
 
 
 def user_managed_clubs(request: HttpRequest) -> dict:
+    if request.path.startswith("/admin/"):
+        return {}
+
     return {
         "user_managed_clubs": get_user_managed_clubs(
             request.user  # type: ignore
