db3b3c01972f2804715b574119819d06b85be310
Marek Dostál <dstlmrk@gmail.com>
2025-10-07 22:46:14 +0200

    Feature: Add tournament detail modal for season fees members

--- ultihub/finance/urls.py
+++ ultihub/finance/urls.py
@@ -6,4 +6,9 @@ app_name = "finance"
 urlpatterns = [
     path("invoices", views.invoices, name="invoices"),
     path("season-fees-list", views.season_fees_list_view, name="season_fees_list"),
+    path(
+        "season-fees-member-detail",
+        views.season_fees_member_detail_view,
+        name="season_fees_member_detail",
+    ),
 ]
